c8b0bc1822b1685e1f8c5d7003da1a7a9312141e
PurvPatel <purv810@gmail.com>
2022-01-15 23:42:35 -0500

    added sat images and composition of said images

--- ImageAnalysis/mapbox.py
+++ ImageAnalysis/mapbox.py
@@ -3,30 +3,81 @@ import requests # The requests package allows use to call URLS
 import shutil   # shutil will be used to copy the image to the local
 import mercantile
 
-lat_lng = [43.640918, -79.371478]
-delta=0.000005
+from PIL import Image
+import math
+from os import listdir
+from os.path import isfile, join
+
+lat_lng = [43.640919, -79.371478]
+delta=0.05
 north = lat_lng[0]+delta
 south = lat_lng[0]-delta
 east = lat_lng[1]+delta
 west = lat_lng[1]-delta
 
-# tl = [lat_lng[0]+delta, lat_lng[1]-delta]
-# br = [lat_lng[0]-delta, lat_lng[1]+delta]
 
-# mercantile.xy()
-
-allTiles = mercantile.tiles(east=east, south=south, north=north, west=west, zooms=18)
+allTiles = mercantile.tiles(east=east, south=south, north=north, west=west, zooms=16)
 counter = 0
 for t in allTiles:
     counter += 1
     r =requests.get('https://api.mapbox.com/v4/mapbox.satellite/'+str(t.z)+'/'+str(t.x)+'/'+str(t.y)+'@2x.pngraw?access_token=pk.eyJ1IjoibW9zdGFmYXRoZXJlYWwiLCJhIjoiY2t5Z2s5cW14MHRnODMxcGw4eTd1bjhtMCJ9.wdYeqrlK23OWOhGuyWl-Wg', stream=True)
 
-    with open('/Users/mostafamohsen/Documents/uni/hacks/EnviroHack/ImageAnalysis/satellite_images/' + str(t.x) + '.' + str(t.y) + '.png', 'wb') as f:
+    with open('./satellite_images/' + str(t.x) + '.' + str(t.y) + '.png', 'wb') as f:
         r.raw.decode_content = True
         shutil.copyfileobj(r.raw, f)
 
 
 ic(counter)
+
+x_tile_range = [0, 0]
+y_tile_range = [0, 0]
+for i, t in enumerate(allTiles):
+    if i == 0:
+        x_tile_range[0] = t.x
+        y_tile_range[0] = t.y
+
+    x_tile_range[1] = t.x
+    y_tile_range[1] = t.y
+
+# Loop over the elevation and satellite image set
+for img_name in ['satellite']:
+
+    # Make a list of the image names
+    image_files = ['./' + img_name + '_images/' + f for f in listdir('./' + img_name + '_images/')]
+
+    # Open the image set using pillow
+    images = [Image.open(x) for x in image_files]
+
+   # Calculate the number of image tiles in each direction
+    edge_length_x = x_tile_range[1] - x_tile_range[0]
+    edge_length_y = y_tile_range[1] - y_tile_range[0]
+
+    edge_length_x = max(1, edge_length_x)
+    edge_length_y = max(1, edge_length_y)
+
+    # Find the final composed image dimensions
+    width, height = images[0].size
+    total_width = width * edge_length_x
+    total_height = height * edge_length_y
+
+    # Create a new blank image we will fill in
+    composite = Image.new('RGB', (total_width, total_height))
+
+    # Loop over the x and y ranges
+    y_offset = 0
+    for i in range(0,edge_length_x):
+        x_offset = 0
+        for j in range(0,edge_length_y):
+
+            # Open up the image file and paste it into the composed image at the given offset position
+            tmp_img = Image.open('./' + img_name + '_images/' + str(i) + '.' + str(j) + '.png')
+            composite.paste(tmp_img, (y_offset,x_offset))
+            x_offset += width # Update the width
+
+        y_offset += height # Update the height
+
+# Save the final image
+composite.save('./composite_images/'+ img_name + '.png')
 
 
 # # Loop over the tile ranges
